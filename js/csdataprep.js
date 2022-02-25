444b8bd75ab8fe9fa6a5d07319f138f0287e4c11
Durgendra <dpsmad999@gmail.com>
2022-02-25 10:35:52 +0530

    as per commodity data

--- js/csdataprep.js
+++ js/csdataprep.js
@@ -1,54 +1,79 @@
 function genType(d) {
-    d.TIMESTAMP  = parseDate(d.TIMESTAMP);
-    d.LOW        = +d.LOW;
-    d.HIGH       = +d.HIGH; 
-    d.OPEN       = +d.OPEN;
-    d.CLOSE      = +d.CLOSE;
-    d.TURNOVER   = +d.TURNOVER;
-    d.VOLATILITY = +d.VOLATILITY;
-    return d;
+  d.price_date = parseDate(d.price_date);
+  d.min_price = +d.min_price;
+  d.max_price = +d.max_price;
+  d.OPEN = +d.OPEN;
+  d.CLOSE = +d.CLOSE;
+  d.TURNOVER = +d.TURNOVER;
+  d.VOLATILITY = +d.VOLATILITY;
+  return d;
+}
+
+function timeCompare(date, interval) {
+  if (interval == "week") {
+    var durfn = d3.time.monday(date);
+  } else if (interval == "month") {
+    var durfn = d3.time.month(date);
+  } else {
+    var durfn = d3.time.day(date);
   }
-  
-  function timeCompare(date, interval) {
-    if (interval == "week")       { var durfn = d3.time.monday(date); }
-    else if (interval == "month") { var durfn = d3.time.month(date); }
-    else { var durfn = d3.time.day(date); } 
-    return durfn;
-  }
-  
-  function dataCompress(data, interval) {
-    var compressedData  = d3.nest()
-                   .key(function(d) { return timeCompare(d.TIMESTAMP, interval); })
-                   .rollup(function(v) { return {
-                           TIMESTAMP:   timeCompare(d3.values(v).pop().TIMESTAMP, interval),
-                           OPEN:        d3.values(v).shift().OPEN,
-                           LOW:         d3.min(v, function(d) { return d.LOW;  }),
-                           HIGH:        d3.max(v, function(d) { return d.HIGH; }),
-                           CLOSE:       d3.values(v).pop().CLOSE,
-                           TURNOVER:    d3.mean(v, function(d) { return d.TURNOVER; }),
-                           VOLATILITY:  d3.mean(v, function(d) { return d.VOLATILITY; })
-                          }; })
-                   .entries(data).map(function(d) { return d.values; });
-  
-    return compressedData;
-  }
-  // csheader.js
-  function csheader() {
-  
+  return durfn;
+}
+
+function dataCompress(data, interval) {
+  var compressedData = d3
+    .nest()
+    .key(function (d) {
+      return timeCompare(d.price_date, interval);
+    })
+    .rollup(function (v) {
+      return {
+        price_date: timeCompare(d3.values(v).pop().price_date, interval),
+        OPEN: d3.values(v).shift().OPEN,
+        min_price: d3.min(v, function (d) {
+          return d.min_price;
+        }),
+        max_price: d3.max(v, function (d) {
+          return d.max_price;
+        }),
+        CLOSE: d3.values(v).pop().CLOSE,
+        TURNOVER: d3.mean(v, function (d) {
+          return d.TURNOVER;
+        }),
+        VOLATILITY: d3.mean(v, function (d) {
+          return d.VOLATILITY;
+        }),
+      };
+    })
+    .entries(data)
+    .map(function (d) {
+      return d.values;
+    });
+
+  return compressedData;
+}
+// csheader.js
+function csheader() {
   function cshrender(selection) {
-    selection.each(function(data) {
-    
-      var interval   = TIntervals[TPeriod];
-      var format     = (interval=="month")?d3.time.format("%b %Y"):d3.time.format("%b %d %Y");
-      var dateprefix = (interval=="month")?"Month of ":(interval=="week")?"Week of ":"";
-      d3.select("#infodate").text(dateprefix + format(data.TIMESTAMP));
+    selection.each(function (data) {
+      var interval = TIntervals[TPeriod];
+      var format =
+        interval == "month"
+          ? d3.time.format("%b %Y")
+          : d3.time.format("%b %d %Y");
+      var dateprefix =
+        interval == "month"
+          ? "Month of "
+          : interval == "week"
+          ? "Week of "
+          : "";
+      d3.select("#infodate").text(dateprefix + format(data.price_date));
       d3.select("#infoopen").text("O " + data.OPEN);
-      d3.select("#infohigh").text("H " + data.HIGH);
-      d3.select("#infolow").text("L " + data.LOW);
+      d3.select("#infohigh").text("H " + data.max_price);
+      d3.select("#infolow").text("L " + data.min_price);
       d3.select("#infoclose").text("C " + data.CLOSE);
-  
     });
   } // cshrender
-  
+
   return cshrender;
-  } // csheader
+} // csheader
